8c391d3befc7a99dae892338a68a928d17e15453
Schubert Anselme <sanselme@icloud.com>
2016-04-27 16:32:08 -0400

    Adding brew cask outdated/upgrade

--- profile.d/cask.sh
+++ profile.d/cask.sh
@@ -1,1 +1,20 @@
 export HOMEBREW_CASK_OPTS="--appdir=/Applications --prefpanedir=/Library/PreferencePanes --qlplugindir=/Library/QuickLook --fontdir=/Library/Fonts --input_methoddir=/Library/Input\ Methods --screen_saverdir=/Library/Screen\ Savers"
+
+action() {
+  caskapps=`ls /opt/homebrew-cask/Caskroom`
+
+  for app in ${caskapps}; do
+    apptocheck=`brew cask info ${app} | awk /installed/`
+
+    if [[ -n ${apptocheck} ]]; then
+      case $1 in
+        outdated) echo ${app};;
+        update)brew cask install --force ${app};;
+        *) break;;
+      esac
+    fi
+  done
+}
+
+alias brew_cask_outdated="action outdated"
+alias brew_cask_upgrade="action update"
