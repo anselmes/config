9dee028ce98b720375941df4ac249d2b6a434ba2
Schubert Anselme <sanselme@icloud.com>
2016-06-16 13:08:11 -0400

    Updating homebrew cask location

--- profile.d/cask.sh
+++ profile.d/cask.sh
@@ -1,7 +1,8 @@
 export HOMEBREW_CASK_OPTS="--appdir=/Applications --prefpanedir=/Library/PreferencePanes --qlplugindir=/Library/QuickLook --fontdir=/Library/Fonts --input_methoddir=/Library/Input\ Methods --screen_saverdir=/Library/Screen\ Savers"
 
 action() {
-  caskapps=`ls /opt/homebrew-cask/Caskroom`
+  caskappsdir=/usr/local/Caskroom
+  caskapps=`ls ${caskappsdir}`
 
   for app in ${caskapps}; do
     apptocheck=`brew cask info ${app} | awk /installed/`
@@ -9,7 +10,7 @@ action() {
     if [[ -n ${apptocheck} ]]; then
       case $1 in
         outdated) echo ${app};;
-        update)brew cask uninstall ${app} && brew cask install --force ${app};;
+        update)rm -rf ${caskappsdir}/${app}/* && brew cask install --force ${app};;
         *) break;;
       esac
     fi
